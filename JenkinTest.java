cde461770547855e4f0b0a94cd6f9a9dee3dba2c
kamranjamali <kamranjamali365@gmail.com>
2023-09-07 17:37:36 +0500

    asa
    asa

--- JenkinTest.java
+++ JenkinTest.java
@@ -1,9 +1,5 @@
-import java.util.Scanner;
-
 public class JenkinTest{
     public static void main(String ars[]){
-        Scanner scanner=new Scanner(System.in);
-        scanner.nextInt();
         System.out.println("Helllo World");
         System.out.println("Good BYe");
     }
