86582149b9f649ccdca80f9717fae396e25b98af
kamranjamali <kamranjamali365@gmail.com>
2023-09-07 17:35:29 +0500

    as

--- JenkinTest.java
+++ JenkinTest.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class JenkinTest{
     public static void main(String ars[]){
+        Scanner scanner=new Scanner(System.in);
+        scanner.nextInt();
         System.out.println("Helllo World");
         System.out.println("Good BYe");
     }
